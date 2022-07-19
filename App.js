2a14dec6310426507d2fe47ad8895e389377dca5
linds0202 <lindsayarainey@gmail.com>
2022-07-19 16:16:04 -0700

    complete airbnb experiences clone

--- App.js
+++ App.js
@@ -2,22 +2,15 @@ import React from "react"
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './data'
 
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
 
 export default function App() {
     const cards = data.map(item => {
         return (
             <Card 
-                img={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
+                key={item.id}
+                {...item}
             />
         )
     })        
--- components/Card.js
+++ components/Card.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react"
 
-export default function Card (props) {
+export default function Card(props) {
     let badgeText
     if (props.openSpots === 0) {
         badgeText = "SOLD OUT"
     } else if (props.location === "Online") {
         badgeText = "ONLINE"
     }
+    
     return (
-        <div className='card'>
+        <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`../images/${props.img}`} className="card--image" />
+            <img src={`../images/${props.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
             <p className="card--title">{props.title}</p>
             <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
